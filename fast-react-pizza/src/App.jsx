288099dd82bcb135e49823e76a9835e271ec05d0
Jaimeen Patel <jaimeenpatel2017@gmail.com>
2025-01-12 11:59:16 +0530

    create folder structure

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,5 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from './features/cart/Cart.jsx';
+import Order from './features/order/Order.jsx';
+import CreateOrder from './features/order/CreateOrder.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
