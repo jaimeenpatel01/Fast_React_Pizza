ec683e7045f742d92d9d2718135df75fbf502dff
Jaimeen Patel <jaimeenpatel2017@gmail.com>
2025-01-14 21:59:17 +0530

    319- Deleteing Cart Items

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
+  const quantity = useSelector(getTotalCartQuantity);
+  const price = useSelector(getTotalCartPrice);
+  if (!quantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>
+          {quantity}
+          {quantity === 1 ? " pizza" : " pizzas"}{" "}
+        </span>
+        <span>${price}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
